36ef2241c7c20e429dac3960a3a7680894f19515
max Icolumi <max77648@gmail.com>
2021-05-09 21:57:45 +0300

    Добавил считывание из файла

--- MaximumInMuseum/MaximumInMuseum.cpp
+++ MaximumInMuseum/MaximumInMuseum.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <fstream>
+#include <vector>
 
 class Time
 {
@@ -11,12 +13,12 @@ private:
 
 	bool IsCorrectHours(size_t hour)
 	{
-		return hour >= 0 && hour <= 24;
+		return hour >= 0 && hour < 24;
 	}
 
 	bool IsCorrectMinutes(size_t minutes)
 	{
-		return minutes >= 0 && minutes <= 60;
+		return minutes >= 0 && minutes < 60;
 	}
 
 	void ShowZeroIf(size_t value)
@@ -41,7 +43,7 @@ public:
 		}
 		else
 		{
-			throw - 1;
+			throw "Проблема с созданием Time, проверьте входные параметры!\n";
 		}
 	}
 
@@ -49,20 +51,58 @@ public:
 	{
 		return _hour == t._hour && _minute == t._minute && _second == t._second;
 	}
-	
+
 	bool operator != (Time t)
 	{
 		return _hour != t._hour || _minute != t._minute || _second != t._second;
 	}
-	
+
 	bool operator > (Time t)
 	{
-		return _hour > t._hour || _minute > t._minute || _second > t._second;
+		if(_hour > t._hour)
+		{
+			if(_minute > t._minute)
+			{
+				return _second > t._second;
+			}
+		}
+		return  false;
 	}
-	
+
 	bool operator < (Time t)
 	{
-		return _hour < t._hour || _minute < t._minute || _second < t._second;
+		if(_hour < t._hour)
+		{
+			if(_minute < t._minute)
+			{
+				return _second < t._second;
+			}
+		}
+		return  false;
+	}
+
+	bool operator <= (Time t)
+	{
+		if(_hour <= t._hour)
+		{
+			if(_minute <= t._minute)
+			{
+				return _second <= t._second;
+			}
+		}
+		return  false;
+	}
+
+	bool operator >= (Time t)
+	{
+		if(_hour >= t._hour)
+		{
+			if(_minute >= t._minute)
+			{
+				return _second >= t._second;
+			}
+		}
+		return  false;
 	}
 
 	void ShowInfo()
@@ -100,8 +140,17 @@ public:
 
 	Pair(Time come, Time getOut)
 	{
-		_come = come;
-		_getOut = getOut;
+		if(come >= getOut)
+		{
+			come.ShowInfo(); std::cout << '\n';
+			getOut.ShowInfo(); std::cout << '\n';
+			throw "Время прихода больше или равно чем время ухода!\n";
+		}
+		else
+		{
+			_come = come;
+			_getOut = getOut;
+		}
 	}
 
 	void ShowInfo()
@@ -123,17 +172,123 @@ public:
 	}
 };
 
+bool IsCorrectExtention(std::string& filename, std::string  extention = ".txt")
+{
+	try
+	{
+		for(int i = extention.size() - 1, j = filename.size() - 1; i >= 0; i--, j--)
+		{
+			if(extention[i] != filename[j])
+			{
+				return false;
+			}
+		}
+	}
+	catch(const std::exception&)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+//00:00:00
+Time StringToTime(std::string s)
+{
+	std::string hour = "";
+	hour += s[0];
+	hour += s[1];
+
+	std::string min = "";
+	min += s[3];
+	min += s[4];
+
+	std::string sec = "";
+	sec += s[6];
+	sec += s[7];
+
+	return Time(stoi(hour), stoi(min), stoi(sec));
+}
+
+std::vector<Pair> ParseFromFile(std::ifstream& outFile)
+{
+	std::string line;
+	std::vector<Pair> times;
+	Time come;
+	Time leave;
+	std::string timeInStr = "";
+
+	while(std::getline(outFile, line))
+	{
+		for(size_t i = 0; i < line.size(); i++)
+		{
+			if(line[i] != ' ')
+			{
+				timeInStr += line[i];
+			}
+			else
+			{
+				come = StringToTime(timeInStr);
+				timeInStr.clear();
+			}
+		}
+		leave = StringToTime(timeInStr);
+		times.push_back(Pair(come, leave));
+		timeInStr.clear();
+	}
+	outFile.close();
+	return times;
+}
+
+void ReadFromFile()
+{
+	std::string input;
+	bool isCorrect = false;
+	std::ifstream outFile;
+	while(isCorrect == false)
+	{
+		std::cout << "Введите имя файла с раширением txt\n";
+		std::cout << "Пример верного файла с расширением: input.txt\n";
+		std::cin >> input;
+
+		if(IsCorrectExtention(input))
+		{
+			outFile.open(input);
+			if(outFile.is_open())
+			{
+				system("cls");
+				try
+				{
+					std::vector<Pair> times = ParseFromFile(outFile);
+					for(size_t i = 0; i < times.size(); i++)
+					{
+						times[i].ShowInfo();
+					}
+				}
+				catch(const char* str)
+				{
+					std::cout << str;
+				}
+				isCorrect = true;
+			}
+			else
+			{
+				std::cout << "Такого файла не сущестует!\n";
+			}
+		}
+		else
+		{
+			std::cout << "Неверное расширение!\n";
+		}
+	}
+}
+
 using namespace std;
 int main()
 {
-	Time a(1, 1, 1);
-	Time b(1, 2, 3);
-	a.ShowInfo(); cout << endl;
-	b.ShowInfo(); cout << endl;
-	cout << (a == b) << endl;
-	cout << (a != b) << endl;
-	cout << (a > b) << endl;
-	cout << (a < b) << endl;
+	setlocale(LC_ALL, "rus");
+
+	ReadFromFile();
 	system("pause");
 	return 0;
 }
